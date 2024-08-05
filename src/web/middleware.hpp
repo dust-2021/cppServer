068db27e1e801794783b4f70b37b12dc2b3dd0f3
libo <277731896@qq.com>
2024-08-06 01:30:30 +0800

    请求上下文

--- src/web/middleware.hpp
+++ src/web/middleware.hpp
@@ -5,23 +5,63 @@
 #include "map"
 #include "regex"
 
+// 请求上下文
+class r_context {
+public:
+    // 请求体
+    http::request<http::string_body> req;
+    // 响应体
+    http::response<http::string_body> res;
+    // 上下文变量
+    std::unordered_map<const char *, std::any> context;
+    // 目标路由
+    const char* route = "";
+
+    const char* method;
+
+    explicit r_context(http::request<http::string_body >& req): req(req){
+        res = http::response<http::string_body>{};
+        method = req.method_string().data();
+    }
+
+    // 获取上下文变量
+    template<class T>
+    T get(const char *key, T&& default_v) {
+        auto it = context.find(key);
+        if (it == context.end()) {
+            return default_v;
+        }
+        return static_cast<T>(it->second);
+    }
+
+    // 设置上下文变量
+    template<class T>
+    void set(const char *key, T &&value) {
+        context[key] = std::any(value);
+    }
+
+    bool exited = false;
+};
+
 class middleware {
 public:
 
     // 子类必须重载仿函数
-    virtual http::request<http::string_body> operator()(http::request<http::string_body> &req) = 0;
+    virtual void operator()(r_context& ctx) = 0;
 };
 
 class parser : public middleware {
 public:
     static const std::regex parse;
 
-    http::request<http::string_body> operator()(http::request<http::string_body> &req) override {
+    void operator()(r_context &ctx) override {
         std::cmatch match;
-        if (std::regex_search(req.target().data(), match, parse)){
+        auto org_route = ctx.req.target().data();
+        if (std::regex_search(org_route, match, parse)){
 
+        } else {
+            ctx.route = org_route;
         }
-        return req;
     };
 };
 
--- src/web/routers.hpp
+++ src/web/routers.hpp
@@ -3,45 +3,19 @@
 
 #include "init.hpp"
 #include "middleware.hpp"
-#include "nlohmann/json.hpp"
 #include "server.hpp"
 #include "unordered_map"
-
-// 请求上下文
-class r_context {
-public:
-    http::request<http::string_body> req;
-    std::unordered_map<const char *, std::any> context;
-
-    template<class T>
-    T get(const char *key, T default_v = NULL) {
-        auto it = context.find(key);
-        if (it == context.end()) {
-            return default_v;
-        }
-        return static_cast<T>(it->second);
-    }
-
-    template<class T>
-    void set(const char *key, T &&value) {
-        context[key] = std::any(value);
-    }
-
-    bool exited = false;
-};
 
 // 路由注册基类
 class route {
 public:
-    template<typename... Args>
     explicit route(const char *r) : r(r) {
     };
 
+    virtual void get() { };
 
+    virtual void operator()(r_context &ctx) final {
 
-    virtual http::response<http::string_body> operator()(http::request<http::string_body>) final {
-        auto res = http::response<http::string_body>{};
-        return res;
     };
 
     const char *r;
